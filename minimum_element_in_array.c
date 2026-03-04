0de8870af7a81b4eaf0d0b8ce9d4068d6af72e0a
madhusd17 <madhusomashekhard@gmail.com>
2026-03-04 21:37:01 +0530

    Added minimum element in an array program

---
+++ minimum_element_in_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, min;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    min = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
